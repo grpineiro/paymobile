c9687e93e7d9b214cb6dd47645d8936c5ef35290
Gabriel Piñeiro <grpineiro@gmail.com>
2021-04-22 23:02:09 -0300

    The routes were divided according to each screen grouping.

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
-import Routes from './src/routes';
+import LoginRoutes from './src/routes/login.routes';
 import globalStyle from './src/styles/global';
 
 export default function App() {
   return (
     <View style={globalStyle.container}>
       <StatusBar style="auto" />
-      <Routes />
+      <LoginRoutes />
     </View>
   );
 }
